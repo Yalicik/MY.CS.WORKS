8cf45ef3c434295a876d3e5fb9b669615cf0dcc7
Yalicik <112151765+Yalicik@users.noreply.github.com>
2022-12-18 14:35:38 +0300

    Create Hypotenuse.cs

---
+++ Hypotenuse.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ALLAHDIYENTURBISON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side A");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt(a*a + b*b);
+            Console.WriteLine("The hypotenious is " + c);
+            Console.ReadKey();
+
+        }
+    }
+}
+
